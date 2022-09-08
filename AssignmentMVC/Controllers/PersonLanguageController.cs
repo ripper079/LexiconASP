d579c52d76f4c07849829078b7d1d1f618385d3d
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-09-08 10:55:37 +0200

    Add PersonLanguage Controller for joining table

---
+++ AssignmentMVC/Controllers/PersonLanguageController.cs
@@ -0,0 +1,28 @@
+﻿using AssignmentMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using AssignmentMVC.Models;
+using AssignmentMVC.Data;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentMVC.Controllers
+{
+    public class PersonLanguageController : Controller
+    {
+        /*private*/
+        readonly ApplicationDbContext _context; //skapar en readonly av DbContext
+
+        public PersonLanguageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Person> personLanguageSkills = _context.People.Include(x => x.Languages).ToList();
+
+            return View(personLanguageSkills);
+        }
+    }
+}
