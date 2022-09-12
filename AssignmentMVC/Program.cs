a2ea3ac8cad09c1f34218a09b2367272dbea9772
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-09-12 12:21:28 +0200

    Program.cs Setup/config for Identity

--- AssignmentMVC/Program.cs
+++ AssignmentMVC/Program.cs
@@ -1,5 +1,7 @@
 using AssignmentMVC.Data;
 using Microsoft.EntityFrameworkCore;
+using AssignmentMVC.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +16,28 @@ builder.Services.AddSession(options =>
 
 //Add entity framework service support, configure/setup for Dependency Injection
 builder.Services.AddDbContext<ApplicationDbContext>(options => {                                 //What db to use
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));       //Use DefaultConnection, must match with Program.cs
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));       //Use DefaultConnection, must match with appsettings.json
 });
 
+
+//Identity config - Use ApplicationUser for Identity[Customizing identity] (insteed of the built in IdentityUser)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                                                    .AddRoles<IdentityRole>()
+                                                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+//Custom config for identity
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireLowercase  = true;
+    options.Password.RequiredUniqueChars = 1;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+});
+
+//For identity support
+builder.Services.AddRazorPages();
 
 //The app
 var app = builder.Build();
@@ -29,6 +50,15 @@ app.UseStaticFiles();
 
 //Enable routing
 app.UseRouting();
+
+//Authentication support - This code line BEFORE Authorization
+app.UseAuthentication();
+//Authorization support
+app.UseAuthorization();
+//For identity
+app.MapRazorPages();
+
+
 
 //Define some routings
 //Nr 1
