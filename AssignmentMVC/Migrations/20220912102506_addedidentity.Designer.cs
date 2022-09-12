df10b65a2b98ccdc39d65f9fa966dbe47a1ffb9d
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-09-12 12:29:16 +0200

    After Update-Database for identity

--- AssignmentMVC/Migrations/20220912102506_addedidentity.Designer.cs
+++ AssignmentMVC/Migrations/20220912102506_addedidentity.Designer.cs
@@ -4,6 +4,7 @@ using AssignmentMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AssignmentMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220912102506_addedidentity")]
+    partial class addedidentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
