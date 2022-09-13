20dff25fe49677b19f974aaa263aaaa94336df42
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-09-13 08:13:57 +0200

    Create view model - CreateRoleViewModel

--- AssignmentMVC/Controllers/RoleController.cs
+++ AssignmentMVC/Controllers/RoleController.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AssignmentMVC.Controllers
 {
     public class RoleController : Controller
     {
+        readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        //This will display the form create new Role
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int thisShouldBeAViewModel) 
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
