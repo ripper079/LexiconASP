41cf8da1a1c1f87e8688f1bcfcb140f6114bd177
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-10-03 13:10:54 +0200

    Added React Controller

---
+++ AssignmentMVC/Controllers/ReactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReactController : ControllerBase
+    {
+    }
+}
